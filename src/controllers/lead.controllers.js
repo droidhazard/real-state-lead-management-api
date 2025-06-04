eeef378f4d07f62433218d11c579c76647509d7b
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-06-04 23:17:37 +0500

    /POST lead working

--- src/controllers/lead.controllers.js
+++ src/controllers/lead.controllers.js
@@ -1,6 +1,38 @@
-function createLead(req, res) {
-  console.log("success");
-  res.send({ msg: "success" });
+import mongoose from "mongoose";
+import { Lead } from "../models/user.model.js";
+async function createLead(req, res) {
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    message,
+    label,
+    estimatedAmount,
+    company,
+    country,
+    city,
+    state,
+    street,
+    notes,
+  } = req.body;
+
+  const newLead = await Lead.create({
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    message,
+    label,
+    estimatedAmount,
+    company,
+    country,
+    city,
+    state,
+    street,
+    notes,
+  });
+  res.status(200).json({ msg: "success", lead: newLead });
 }
 
 export { createLead };
