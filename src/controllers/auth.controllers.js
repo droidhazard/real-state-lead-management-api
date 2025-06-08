c4ebb1a867785b3310c66cd07f03722cac56ba56
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-06-08 16:45:22 +0500

    add token generation and returned in response

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,9 +1,13 @@
 import { User } from "../models/user.model.js";
 import { validationResult } from "express-validator";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const registerUser = async function (req, res) {
   try {
+    // * Validate request data
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res
@@ -13,12 +17,14 @@ const registerUser = async function (req, res) {
 
     const { username, email, password } = req.body;
 
+    // * Check if user doesn't already exist
     const userExists = await User.findOne({ email });
     console.log(userExists);
     if (userExists) {
       res.status(400).json({ message: "user with this email already exists." });
     }
 
+    // * Generate hashed password and Create User
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
@@ -29,6 +35,7 @@ const registerUser = async function (req, res) {
 
     const userData = await user.save();
 
+    // * Send response
     return res.status(201).json({
       message: "user created successfully",
       success: true,
@@ -49,7 +56,24 @@ const loginUser = async function (req, res) {
       .status(404)
       .json({ message: "No account with this email found." });
   }
-  res.status(200).json({ message: "logged in." });
+
+  // * Check if password provided in request matches user's
+  const hashedPassword = userExists?.password;
+  const passwordMatched = await bcrypt.compare(password, hashedPassword);
+
+  if (!passwordMatched) {
+    res.status(401).json({ message: "email or password is incorrect." });
+  }
+
+  // * Generate Token
+  const token = jwt.sign(
+    { userId: userExists._id },
+    process.env.JWT_SIGNING_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  // * Send Response
+  res.status(200).json({ message: "logged in.", access_token: token });
 };
 
 // module.exports = { registerUser };
